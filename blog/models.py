14ccda51ed41c0117362cf1530307f9edb5dafd9
xizo <xizo@django.com>
2020-02-19 23:42:46 +0200

    modified temps to dis posts fr dp

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -13,7 +12,7 @@ class Post(models.Model):
 
     def publish(self):
         self.published_date = timezone.now()
-        Self.save()
+        self.save()
 
     def __str__(self):
         return self.title
